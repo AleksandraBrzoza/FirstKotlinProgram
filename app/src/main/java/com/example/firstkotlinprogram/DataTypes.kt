46201aa7970dbedfbceb846c8f05647a02f288aa
Aleksandra Brzoza <alebrz00104@gmail.com>
2025-02-20 07:57:33 +0100

    edit Variables file and add DataTypes file

---
+++ app/src/main/java/com/example/firstkotlinprogram/DataTypes.kt
@@ -0,0 +1,22 @@
+package com.example.firstkotlinprogram
+
+fun main(args: Array<String>) {
+
+    var a:Boolean = true
+    var b:Char = 'R'
+    var c:Byte = 12
+    var d:Short = -356
+    var e:Int = 43543
+    var f:Long = -43254323L
+    var g:Float = 5.6784324F
+    var h:Double = 7.34543276464
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+}
--- app/src/main/java/com/example/firstkotlinprogram/Variables.kt
+++ app/src/main/java/com/example/firstkotlinprogram/Variables.kt
@@ -1,4 +1,31 @@
 package com.example.firstkotlinprogram
 
-class Variables {
+fun main(args: Array<String>) {
+
+    /*
+    var a = 10
+    var b = 20
+    var c = a + b
+
+    println(a + b)
+
+    var age = 10 // valid
+    var _age = 20 // valid
+    var age3 = 30 // valid
+
+    var a ge = 40 // invalid
+    var 3age = 10 // inwalid
+
+     */
+
+    var age = 20
+    age = 10
+    println(age)
+
+    val year = 2020
+    // year = 2019 // invalid
+
+
+
+
 }
